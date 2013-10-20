5b5d741817e4a1caba5df8838c633373d40c0fa2
wanderanimrod <wanderanimrod@gmail.com>
2013-10-20 19:20:27 +0300

    [nim] making bootstrapping idempotent

--- vagrant-stuff/bootstrap.sh
+++ vagrant-stuff/bootstrap.sh
@@ -10,11 +10,13 @@ echo -e "\n\nInstalling git ..."
 apt-get install git -y
 
 echo -e "\n\nCloning provisioning repository ..."
+rm -r /home/provisioning
 mkdir -p /home/provisioning
 git clone https://github.com/wanderanimrod/infrastructure-as-code--provisioning.git /home/provisioning
 
 echo -e "\n\nConfiguring chef-solo paths ..."
 mkdir -p /etc/chef
+rm -r /etc/chef/solo.rb
 touch /etc/chef/solo.rb
 echo "cookbook_path    \"/home/provisioning/chef/cookbooks\"" >> /etc/chef/solo.rb
 echo "role_path    \"/home/provisioning/chef/roles\"" >> /etc/chef/solo.rb
