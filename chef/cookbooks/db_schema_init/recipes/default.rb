9d63f98de61d5688cc905ce531b4f814721bb15b
wanderanimrod <wanderanimrod@gmail.com>
2013-10-20 20:12:44 +0300

    [nim] Fixing typo

--- chef/cookbooks/db_schema_init/recipes/default.rb
+++ chef/cookbooks/db_schema_init/recipes/default.rb
@@ -1,6 +1,6 @@
 execute "Sync database" do
 	cwd "/home/simple-list-view/app/simple_list_view"
-	command "bash -c 'source /home/simple-list-view/virtualen/bin/activate && ./manage.py syncdb --noinput'"
+	command "bash -c 'source /home/simple-list-view/virtualenv/bin/activate && ./manage.py syncdb --noinput'"
 	action :run
 end
 
